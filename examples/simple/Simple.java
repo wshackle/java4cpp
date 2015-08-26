1cd938dbe9d79722e024e7c5723a83aa3c4ab3aa
Will Shackleford <william.shackleford@nist.gov>
2015-08-26 11:38:34 -0400

    Remove a debug print from simple example.

--- examples/simple/Simple.java
+++ examples/simple/Simple.java
@@ -1,12 +1,12 @@
 
 public class Simple {
-    
+
     final int value;
-    
+
     public Simple(int value) {
         this.value = value;
     }
-    
+
     public int getValue() {
         return value;
     }
--- examples/simple/use_simple_java.cpp
+++ examples/simple/use_simple_java.cpp
@@ -1,16 +1,13 @@
 
-#include "simple.h"
-#include <iostream>
+    #include "simple.h"
+    #include <iostream>
 
-using namespace simple;
-using namespace std;
+    using namespace simple;
+    using namespace std;
 
+    int main(int argc, const  char **argv){
 
-int main(int argc, const  char **argv){
+            Simple s(4);
+            cout << "s.getValue() = " << s.getValue() << endl;
 
-	cout << "C++ main started." << endl;
-
-	Simple s(4);
-	cout << "s.getValue() = " << s.getValue() << endl;
-
-}
+    }
