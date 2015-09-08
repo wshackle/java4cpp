20052307bb5f3969d36bdd75a349917cdcaeed0b
Will Shackleford <william.shackleford@nist.gov>
2015-09-08 16:51:08 -0400

    update run script.

--- crcl4java-utils/run.sh
+++ crcl4java-utils/run.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 
-if test ! -f ./run.sh ; then 
-    cd "${0%%run.sh}";
-fi
-
-if test "x" != "x${JAVA_HOME}" ; then
-    export PATH="${JAVA_HOME}/bin/:${PATH}";
-fi
-
-if test ! -f target/CRCLJava-1.0-SNAPSHOT-jar-with-dependencies.jar ; then
-    mvn -version || ( echo "Please install maven." && false)
-    mvn package
-fi;
-
-java -jar target/CRCLJava-1.0-SNAPSHOT-jar-with-dependencies.jar $*
+../run.sh
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if test ! -f ./run.sh ; then 
+    cd "${0%%run.sh}";
+fi
+
+if test "x" != "x${JAVA_HOME}" ; then
+    export PATH="${JAVA_HOME}/bin/:${PATH}";
+fi
+
+export JARFILE=crcl4java-ui/target/crcl4java-ui-1.0-SNAPSHOT-jar-with-dependencies.jar
+if test ! -f "${JARFILE}" ; then
+    mvn -version || ( echo "Please install maven." && false)
+    mvn package
+fi;
+
+\rm -f run[0-9]*.log run[0-9]*.err >/dev/null 2>/dev/null || true
+
+java -jar "${JARFILE}" $* > run$$.log 2> run$$.err
+
