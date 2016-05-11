c58e1f031b8a067bad7d69349fd618f5deedfea5
Will Shackleford <william.shackleford@nist.gov>
2016-05-11 08:36:59 -0400

    restore test

---
+++ examples/test/java/test/src/main/java/testpackage/ExternalPublicEnum.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testpackage;
+
+/**
+ *
+ * @author shackle
+ */
+public enum ExternalPublicEnum {
+    ONE,TWO,THREE;
+    
+}
